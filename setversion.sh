44a79d8196494cabec5aa6a3e108dd08bf574d49
Łukasz Krenski <sakulk@gmail.com>
2013-02-18 12:07:12 +0100

    new version

--- setversion.sh
+++ setversion.sh
@@ -1,7 +1,7 @@
 #pamietaj o zmianie application.properties recznie w grailsowych appkac
 #juz nie musisz pamietac :p - zolw
-NEW=2.0.2-ZOLW-CUSTOM
-OLD=2.0.1-ZOLW-CUSTOM
+NEW=2.0.3-ZOLW-CUSTOM
+OLD=2.0.2-ZOLW-CUSTOM
 mvn versions:set -DoldVersion=$OLD -DnewVersion=$NEW
 #sed -i "s/app.version=$OLD/app.version=$NEW/g" chimeo-web/application.properties
 #sed -i "s/app.version=$OLD/app.version=$NEW/g" chimeo-web-admin/application.properties
